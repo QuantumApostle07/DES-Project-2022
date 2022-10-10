b3e91f3fce0a8b7585a9d93bf09b201310948d52
justingray1 <juddygray@gmail.com>
2022-10-10 16:13:33 +0800

    Steering

--- GPS_car/GPS_car.ino
+++ GPS_car/GPS_car.ino
@@ -1,86 +1,102 @@
-//Libraries
+/*
+   Created by ArduinoGetStarted.com
+
+   This example code is in the public domain
+
+   Tutorial page: https://arduinogetstarted.com/tutorials/arduino-gps
+*/
+
 #include <PWMServo.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <LSM303.h>
 
-//Variables
 const int RXPin = 4, TXPin = 3;
 const uint32_t GPSBaud = 9600; //Default baud of NEO-6M is 9600
+
+int motorPwmVal = 50;
 const int motorPin = 11;
-const int motorPwmVal = 255;
 
-double currentLat;
-double currentLon;
-double distanceToTarget;
-int targetCourse;
-int currentCourse;
-
-bool A7_state = false;
+float targetCourse;
+float currentCourse;
 
 
-//Library Objects
+TinyGPSPlus gps; // the TinyGPS++ object
+SoftwareSerial gpsSerial(RXPin, TXPin); // the serial interface to the GPS device
+
 PWMServo steerServo;
-TinyGPSPlus gps;
-SoftwareSerial gpsSerial(RXPin, TXPin);
 
-//Target Coordinates
-const double targetLat = -31.98017;
-const double targetLon = 115.81783;
+LSM303 compass;
 
-//Runs Once
+const double target_lat = -31.98015;
+const double target_lon = 115.81791;
+
 void setup() {
   Serial.begin(9600);
   gpsSerial.begin(GPSBaud);
+
   steerServo.attach(SERVO_PIN_A); //pin 9 for arduino nano (handled by library)
 
-  pinMode(A7, INPUT);
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  compass.m_min = (LSM303::vector<int16_t>) {
+    -32767, -32767, -32767
+  };
+  compass.m_max = (LSM303::vector<int16_t>) {
+    +32767, +32767, +32767
+  };
 }
 
-//Runs Infinitely
-void loop()
-{
+void loop() {
 
-  if (analogRead(A7) > 1)
-  {
-    A7_state = HIGH;
-  }
-  else
-  {
-    A7_state = LOW;
-    analogWrite(motorPin, 0);
-  }
 
-  if (A7_state == HIGH)
-  {
-    startMoving();
-  }
-
-  else
-  {
-    stopMoving();
-  }
 
   if (gpsSerial.available() > 0) {
     if (gps.encode(gpsSerial.read())) {
       if (gps.location.isValid()) {
-        currentLat = gps.location.lat();
-        currentLon = gps.location.lng();
-        distanceToTarget = TinyGPSPlus::distanceBetween(currentLat, currentLon, targetLat, targetLon);
-        targetCourse = TinyGPSPlus::courseTo(gps.location.lat(), gps.location.lng(), targetLat, targetLon);
-        currentCourse = gps.course.deg();
+        double latitude = gps.location.lat();
+        double longitude = gps.location.lng();
+        unsigned long distanceToTarget = TinyGPSPlus::distanceBetween(latitude, longitude, target_lat, target_lon);
+
+        //        Serial.print(F("- latitude: "));
+        //        Serial.println(latitude, 8);
+        //
+        //        Serial.print(F("- longitude: "));
+        //        Serial.println(longitude, 8);
+
+        //        Serial.print(F("- distance to target: "));
+        //        Serial.println(distanceKm, 8);
+
+        //        Serial.print(F("speed in km/h: "));
+        //        Serial.println(gps.speed.kmph());
+
+        Serial.print("course to target: ");
+        targetCourse =TinyGPSPlus::courseTo(gps.location.lat(), gps.location.lng(), target_lat, target_lon);
+        Serial.print(targetCourse);
+
+        compass.read();
+        currentCourse = compass.heading();
+        Serial.print(" Current Course: ");
+        Serial.println(currentCourse);
+
+
+        //        Serial.print(F("current course: "));
+        //        Serial.println(gps.course.deg());
 
         if (currentCourse < targetCourse)
         {
-          turnSlightLeft();
-          delay(500);
-          goStraight();
+          turnLeft();
+//          delay(500);
+//          goStraight();
         }
 
         else if (currentCourse > targetCourse)
         {
-          turnSlightRight();
-          delay(500);
-          goStraight();
+          turnRight();
+//          delay(500);
+//          goStraight();
         }
 
         else if (abs((currentCourse - targetCourse)) < 10)
@@ -88,26 +104,30 @@ void loop()
           goStraight();
         }
 
-        else if (distanceToTarget < 5)
+        if (distanceToTarget < 10)
+        {
+          motorPwmVal = 100;
+        }
+
+        else if (distanceToTarget < 3)
+        {
+          motorPwmVal = 50;
+        }
+        else if (distanceToTarget == 0)
         {
           stopMoving();
         }
-        //        Serial.print("target course: ");
-        //        Serial.print(targetCourse);
-        //        Serial.print(" current course: ");
-        //        Serial.println(currentCourse);
 
-        Serial.print("course to target: ");
-        Serial.print(TinyGPSPlus::courseTo(gps.location.lat(), gps.location.lng(), targetLat, targetLon));
-        Serial.print(F(" current course: "));
-        Serial.println(gps.course.deg());
-      }
-      else
-      {
+      } else {
         Serial.println(F("- location: INVALID"));
       }
+
+      Serial.println();
     }
   }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
 }
 
 void goStraight()
@@ -119,35 +139,35 @@ void goStraight()
 void turnSlightLeft()
 {
   steerServo.write(80);
-  Serial.println("Turn Left SLightly");
+  //Serial.println("Turn Left SLightly");
 }
 
 void turnSlightRight()
 {
   steerServo.write(100);
-  Serial.println("Turn Right Slightly");
+  //Serial.println("Turn Right Slightly");
 }
 
 void turnLeft()
 {
   steerServo.write(70);
-  Serial.println("Turn Left");
+  //Serial.println("Turn Left");
 }
 
 void turnRight()
 {
   steerServo.write(110);
-  Serial.println("Turn Right");
+  //Serial.println("Turn Right");
 }
 
 void startMoving()
 {
   analogWrite(motorPin, motorPwmVal);
-  Serial.println("Start Moving");
+  //Serial.println("Start Moving");
 }
 
 void stopMoving()
 {
   analogWrite(motorPin, 0);
-  Serial.println("Stop Moving");
+  //Serial.println("Stop Moving");
 }
